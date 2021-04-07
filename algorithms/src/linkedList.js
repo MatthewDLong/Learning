cbd34448ddd33912fc1393a94da9df08644db9a3
Matthew Long <matthew.long@bbc.co.uk>
2021-04-07 21:06:35 +0100

    Add traverse function to linkedList

--- algorithms/src/linkedList.js
+++ algorithms/src/linkedList.js
@@ -11,3 +11,21 @@ let c = new Node("baz");
 
 a.next = b;
 b.next = c;
+
+var head = a;
+
+function traverse(head) {
+  // always log Node.data
+  console.log(head.data);
+
+  // base case
+  if (!head.next) {
+    return head.data;
+  }
+
+  // recursive case
+  traverse(head.next);
+}
+
+// traverse the linked list, pssing in the first Node
+traverse(a);
