e9fb56ca4f731e6264e48b0448f7836987870b97
Matthew Long <matthew.long@bbc.co.uk>
2021-08-18 23:41:13 +0100

    Add router

--- giphy-search/src/App.js
+++ giphy-search/src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Search from "./Search";
 
 const App = () => {
-  return <Search />;
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
