3da191842e63af79afbceb08c84dfdc9a96dd69a
Matt Long <1992.mdl@gmail.com>
2022-05-20 10:29:44 +0100

    Begin course

---
+++ DSA/udacity-course/python-data-types.py
@@ -0,0 +1,27 @@
+"""Python data types"""
+answer = 10
+answer = "ten"
+on = True
+off = False
+a = 1.1
+multipleTypeList = ['a',1,True]
+multipleTypeTuple = ('a',1,True)
+aDictionary = { 'a': 1, 'b': 2, 'c': 3 }
+foo = None
+
+"""Output"""
+
+print("foo")
+
+"""Conditionals"""
+foo = "bar"
+
+if foo == "bar":
+  print("foo bar")
+elif foo == 'baz':
+  print("foo is baz")
+else:
+  print("qux")
+
+
+"""Loops"""
