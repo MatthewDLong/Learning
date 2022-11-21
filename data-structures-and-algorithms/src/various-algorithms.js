b3d2214c844dfb7af6b6084e087c0c1542d25699
Matt Long <1992.mdl@gmail.com>
2022-11-21 09:41:26 +0000

    Add various algorithms

--- data-structures-and-algorithms/src/various-algorithms.js
+++ data-structures-and-algorithms/src/various-algorithms.js
@@ -155,3 +155,99 @@ runAssertion(() => {
 runAssertion(() => {
   assert.deepEqual(binarySearchRecursive([10, 10, 10], 10), 10);
 });
+
+const binarySearchRecursiveA = (a, target) => {
+  let left = 0;
+  let right = a.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (a[middle] === target) {
+      return a[middle];
+    } else if (a[middle] < target) {
+      return binarySearchRecursiveA(a.slice(middle + 1), target);
+    } else {
+      return binarySearchRecursiveA(a.slice(0, middle), target);
+    }
+  }
+};
+
+runAssertion(() => {
+  assert.equal(
+    binarySearchRecursiveA(
+      [1, 2, 4, 6, 7, 9, 10, 12, 14, 16, 18, 20, 23, 25, 27, 28],
+      9
+    ),
+    9
+  );
+});
+
+/*
+Fibonacci sequence
+
+First 14 integers of Fibonacci Sequence
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+
+Input: n = 0
+Output: 0
+Explanation: F(0) = F(0) + F(0) = 0 + 0 = 0.
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+*/
+
+const fib = (n, lookup) => {
+  lookup = lookup || new Map();
+  if (n === 0) {
+    return 0;
+  } else if (n < 3) {
+    return 1;
+  }
+  if (lookup.has(n)) {
+    return lookup.get(n);
+  } else {
+    const result = fib(n - 1, lookup) + fib(n - 2, lookup);
+    lookup.set(n, result);
+    return result;
+  }
+};
+
+runAssertion(() => {
+  assert.equal(fib((n = 0)), 0);
+  assert.equal(fib((n = 1)), 1);
+  assert.equal(fib((n = 2)), 1);
+  assert.equal(fib((n = 3)), 2);
+  assert.equal(fib((n = 13)), 233);
+});
+
+const longestCommonSubsequence = function (s1, s2) {
+  var table = new Array(s1.length + 1)
+    .fill(0)
+    .map(() => new Array(s2.length + 1).fill(0));
+
+  for (var i = 1; i < table.length; i++) {
+    for (var j = 1; j < table[i].length; j++) {
+      if (s1[i - 1] === s2[j - 1]) {
+        table[i][j] = table[i - 1][j - 1] + 1;
+      } else {
+        table[i][j] = Math.max(table[i - 1][j], table[i][j - 1]);
+      }
+    }
+  }
+  return table[s1.length][s2.length];
+};
+
+runAssertion(() => {
+  assert.equal(longestCommonSubsequence("abcde", "ace"), 3);
+});
